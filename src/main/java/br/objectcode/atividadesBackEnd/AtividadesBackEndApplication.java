6cad50c9916775bac9c13f6c211c0478430fc77a
Bruno Glauco Eles <bgeles@gmail.com>
2019-08-08 11:13:48 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/br/objectcode/atividadesBackEnd/AtividadesBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.objectcode.atividadesBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadesBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/objectcode/atividadesBackEnd/AtividadesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.objectcode.atividadesBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
